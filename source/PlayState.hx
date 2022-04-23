555122f7d38373cb8659def1b779c3bb937322db
NAME!!!!! <stmfebrero@gmail.com>
2022-04-23 16:52:58 +0800

    aaaa

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1143,6 +1143,8 @@ class PlayState extends MusicBeatState
 					startVideo('Cutscene2');
 				case 'crossfire':
 					startVideo('Cutscene3');
+				case 'bullets':
+					startVideo('Cutscene4');
 
 				default:
 					startCountdown();
